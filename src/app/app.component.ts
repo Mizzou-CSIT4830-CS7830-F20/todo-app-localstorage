2983546246f2d394152c74f334a9c411d71f59b0
Professor Wergeles <wergelesn@missouri.edu>
2020-10-15 14:58:27 -0500

    Fixing up the code for before class ... making sure to use Reactive Forms and npm install for material icons instead of changing the index.html file ... using ng add for boostrap instread of adding an external CSS link in the index.html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('moveInLeft', [
+      transition('void=> *', [
+        style({ transform: 'translateX(300px)' }),
+        animate(
+          200,
+          keyframes([
+            style({ transform: 'translateX(300px)' }),
+            style({ transform: 'translateX(0)' }),
+          ])
+        ),
+      ]),
+      transition('*=>void', [
+        style({ transform: 'translateX(0px)' }),
+        animate(
+          100,
+          keyframes([
+            style({ transform: 'translateX(0px)' }),
+            style({ transform: 'translateX(300px)' }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'todo-app-localstorage';
+
+  todoArray = [];
+
+  todoForm = this.fb.group({
+    todo: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  addTodo() {
+    let value = this.todoForm.value.todo;
+    if (value != '') {
+      this.todoArray.push(value);
+      console.log(this.todoArray);
+      this.todoForm.reset();
+    } else {
+      alert('Todo Field Required!!');
+    }
+  }
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+        console.log('delete item');
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
